72d22420f3d87a7855df9c7c289709dca310855f
Dannelysbeth <danielaf@onet.pl>
2024-04-06 11:01:47 +0200

    whole project

---
+++ src/main/java/dannelysbeth/ecommerce/mongodbshop/MongodbShopApplication.java
@@ -0,0 +1,13 @@
+package dannelysbeth.ecommerce.mongodbshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongodbShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MongodbShopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dannelysbeth/ecommerce/mongodbshop/MongodbShopApplicationTests.java
@@ -0,0 +1,13 @@
+package dannelysbeth.ecommerce.mongodbshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MongodbShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
