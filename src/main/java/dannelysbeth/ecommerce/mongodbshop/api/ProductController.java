c7756af3953afce08367975c0a897cf24cb30ea4
Dannelysbeth <danielaf@onet.pl>
2024-05-08 22:29:21 +0200

    added global reesponse

--- src/main/java/dannelysbeth/ecommerce/mongodbshop/api/ProductController.java
+++ src/main/java/dannelysbeth/ecommerce/mongodbshop/api/ProductController.java
@@ -2,16 +2,19 @@ package dannelysbeth.ecommerce.mongodbshop.api;
 
 import dannelysbeth.ecommerce.mongodbshop.mapper.definition.ProductMapper;
 import dannelysbeth.ecommerce.mongodbshop.model.DTO.request.ProductRequest;
+import dannelysbeth.ecommerce.mongodbshop.model.DTO.response.GlobalResponse;
 import dannelysbeth.ecommerce.mongodbshop.model.DTO.response.ProductResponse;
 import dannelysbeth.ecommerce.mongodbshop.model.Product;
 import dannelysbeth.ecommerce.mongodbshop.service.definition.ProductService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.util.StopWatch;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import java.util.Set;
 
@@ -43,20 +46,31 @@ public class ProductController {
     }
 
     @GetMapping
-    public ResponseEntity<Set<ProductResponse>> findAll(@RequestParam(required = false) Double lte,
-                                                        @RequestParam(required = false) Double gte,
-                                                        @RequestParam(defaultValue = "1") Long minQuantity,
-                                                        @RequestParam(required = false) String[] category,
-                                                        @RequestParam(required = false) String[] color,
-                                                        @RequestParam(required = false) String... size) {
+    public ResponseEntity<GlobalResponse> findAll(@RequestParam(required = false) Double lte,
+                                                  @RequestParam(required = false) Double gte,
+                                                  @RequestParam(defaultValue = "1") Long minQuantity,
+                                                  @RequestParam(required = false) String[] category,
+                                                  @RequestParam(required = false) String[] color,
+                                                  @RequestParam(required = false) String... size) {
+
+        StopWatch watch = new StopWatch();
+        watch.start();
+        Set<ProductResponse> responses = productMapper.transformToProductResponse(
+                productService
+                        .getProducts(
+                                gte, lte, minQuantity,
+                                (category == null ? null : Arrays.asList(category)),
+                                (color == null ? null : Arrays.asList(color)),
+                                (size == null ? null : Arrays.asList(size))));
+        watch.stop();
         return ResponseEntity.ok()
-                .body(productMapper
-                        .transformToProductResponse(productService
-                                .getProducts(
-                                        gte, lte, minQuantity,
-                                        (category == null ? null : Arrays.asList(category)),
-                                        (color == null ? null : Arrays.asList(color)),
-                                        (size == null ? null : Arrays.asList(size)))));
+                .body(GlobalResponse.builder()
+                        .count(responses.size())
+                        .entries( Collections.singleton(responses))
+                        .responseTime(watch.getTotalTimeMillis() + "ms")
+                        .build());
+
+
 
     }
 }
